74c07b3e405c1981b01d33db8885812473d0d987
Ruslan Filimonov <filrus85@gmail.com>
2023-03-29 15:44:01 +0300

    change template

--- main/views.py
+++ main/views.py
@@ -2,10 +2,45 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    menu = [{'name': 'Home', 'url': 'index'},
+            {'name': 'Catalogue', 'url': 'index'},
+            {'name': 'FAQ', 'url': 'questions'},
+            {'name': 'Contacts', 'url': 'contacts'},
+            {'name': 'Add Product', 'url': 'add_product'},
+            {'name': 'Add Category', 'url': 'add_category'}] 
+    products = [{'id': 1,
+                'title': 'T-Shirt 1',
+                'articul': '2215',
+                'price': 25}]
+    context = {
+        'menu': menu,
+        'title': 'Home Page',
+        'products': products
+    }
+    return render(request, 'index.html', context=context)
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    menu = [{'name': 'Home', 'url': 'index'},
+            {'name': 'Catalogue', 'url': 'index'},
+            {'name': 'FAQ', 'url': 'questions'},
+            {'name': 'Contacts', 'url': 'contacts'},
+            {'name': 'Add Product', 'url': 'add_product'},
+            {'name': 'Add Category', 'url': 'add_category'}] 
+    context = {
+        'menu': menu,
+        'title': 'Contacts'
+    }
+    return render(request, 'contacts.html', context=context)
 
 def questions(request):
-    return render(request, 'questions.html')
+    menu = [{'name': 'Home', 'url': 'index'},
+            {'name': 'Catalogue', 'url': 'index'},
+            {'name': 'FAQ', 'url': 'questions'},
+            {'name': 'Contacts', 'url': 'contacts'},
+            {'name': 'Add Product', 'url': 'add_product'},
+            {'name': 'Add Category', 'url': 'add_category'}] 
+    context = {
+        'menu': menu,
+        'title': 'FAQ'
+    }
+    return render(request, 'questions.html', context=context)
